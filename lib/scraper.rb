9098c46e10b960b56995458981d74452a2d235d5
james Johnson <2013matthew2013@gmail.com>
2020-03-10 01:32:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++
@@ -1,9 +0,0 @@
-require 'nokogiri'
-require 'open-uri'
-
-html = open("https://flatironschool.com/")
-
-doc = Nokogiri::HTML(html)
-doc.css(".site-header__hero__headline")
-
-puts doc.css(".site-header__hero__headline").text
